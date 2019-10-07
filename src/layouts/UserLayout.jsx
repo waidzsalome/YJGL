ef771521da133660aaf7c16e5d95c910dee21729
waidzsalome <841854755@qq.com>
2019-10-07 21:39:53 +0800

    backstage

---
+++ src/layouts/UserLayout.jsx
@@ -0,0 +1,56 @@
+import { DefaultFooter, getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import DocumentTitle from 'react-document-title';
+import Link from 'umi/link';
+import React from 'react';
+import { connect } from 'dva';
+import { formatMessage } from 'umi-plugin-react/locale';
+import SelectLang from '@/components/SelectLang';
+import oval from '../assets/Oval.png'
+import styles from './UserLayout.less';
+
+const UserLayout = props => {
+  const {
+    route = {
+      routes: [],
+    },
+  } = props;
+  const { routes = [] } = route;
+  const {
+    children,
+    location = {
+      pathname: '',
+    },
+  } = props;
+  const { breadcrumb } = getMenuData(routes);
+  return (
+    <DocumentTitle
+      title={getPageTitle({
+        pathname: location.pathname,
+        breadcrumb,
+        formatMessage,
+        ...props,
+      })}
+    >
+      <div className={styles.container}>
+        <div className={styles.lang}>
+          <SelectLang />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <img alt="logo" className={styles.logo} src={oval} />
+                <span className={styles.title}>后台</span>
+              </Link>
+            </div>
+            <div className={styles.desc}>秦皇岛市应急管理系统</div>
+          </div>
+          {children}
+        </div>
+        <DefaultFooter />
+      </div>
+    </DocumentTitle>
+  );
+};
+
+export default connect(({ settings }) => ({ ...settings }))(UserLayout);
